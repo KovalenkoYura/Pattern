c84685514c92f087ee94e9e80f5075f498e1a711
YuraKovalenko <urakovalenko001@gmail.com>
2023-03-30 23:16:01 +0300

    fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,15 @@
-import patterns.decorator.*;
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.DoubleBinaryOperator;
 
 public class Main {
   public static void main(String[] args) {
-    Beverage beverage = new Espresso();
-    System.out.println(beverage.getDescription() + " " + beverage.cost() + "$");
-    System.out.println("=======================");
-    Beverage beverage1 = new HouseBlend();
-    beverage1 = new Mocha(beverage1);
-    beverage1 = new Soy(beverage1);
-    beverage1 = new Whip(beverage1);
-
-    System.out.println(beverage1.getDescription() + " " + beverage1.cost() + "$");
-    System.out.println("=======================");
-
-    Beverage beverage3 = new Espresso();
-    beverage3 = new Mocha(beverage3);
-    beverage3 = new Mocha(beverage3);
-    beverage3 = new Whip(beverage3);
-
-    System.out.println(beverage3.getDescription() + " " + beverage3.cost() + "$");
-
+    File directory = new File("./src/main/java");
+    String[] names = directory.list((dir, name) -> name.endsWith("java"));
+    System.out.println("Result : ");
+    System.out.println(Arrays.asList(names));
   }
 }
 
